982ef222006a3cf762a64d02a6b0d543aa7e20a5
bouzangad zakaria <bouzangadzakaria@gmail.com>
2024-12-28 20:50:32 +0100

    refactor(NavBar): replace href with Link component for navigation

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,17 +1,18 @@
 "use client";
+import Link from "next/link";
 import { Navbar,Container, Nav } from "react-bootstrap";
 
 export default function NavBar() {
     return (
         <Navbar bg="primary" variant="dark" sticky="top" expand="sm" collapseOnSelect>
             <Container className="justify-content-between">
-                <Navbar.Brand href="/">NextJs Image Gallery</Navbar.Brand>
+                <Navbar.Brand href="/" as={Link}>NextJs Image Gallery</Navbar.Brand>
                 <Navbar.Toggle aria-controls="main-navber"/>
                 <Navbar.Collapse>
                     <Nav className="me-auto">
-                        <Nav.Link href="/hello">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
+                        <Nav.Link href="/hello" as={Link}>Home</Nav.Link>
+                        <Nav.Link href="/about" as={Link}>About</Nav.Link>
+                        <Nav.Link href="/contact" as={Link}>Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
